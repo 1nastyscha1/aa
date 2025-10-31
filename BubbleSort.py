007549e280a89c195c67d4d6581a7c3115f63475
1nastyscha1 <cherneva2929@gmail.com>
2025-10-31 23:44:45 +0300

    Create  BubbleSort.py

---
+++ BubbleSort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+lst = [5, 2, 9, 1, 5, 6]
+bubble_sort(lst)
+print(lst)  # [1, 2, 5, 5, 6, 9]
